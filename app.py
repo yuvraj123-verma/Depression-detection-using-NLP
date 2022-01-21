959d850f51d2fc3fdcd7c4acbd61b3d41db0f5fa
yuvraj123-verma <66717220+yuvraj123-verma@users.noreply.github.com>
2022-01-22 03:01:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+
+# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
+filename = './model/depression2.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+cv = pickle.load(open('./model/cv-transform.pkl','rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	if request.method == 'POST':
+		message = request.form['form-control']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = classifier.predict(vect)
+		return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
